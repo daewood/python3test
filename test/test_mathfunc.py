8704e78422e88fbd38e123a56cd7b7a46e7af951
daewood <daewood@qq.com>
2018-01-14 15:32:41 +0000

    test

---
+++ test/test_mathfunc.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from mathfunc import *
+
+#每个测试方法均以 test 开头，否则是不被unittest识别的
+class TestMathFunc(unittest.TestCase):
+    """Test mathfuc.py"""
+    def setUp(self):
+        print("do something before test.Prepare environment.")
+
+    def tearDown(self):
+        print("do something after test.Clean up.")
+
+    def test_add(self):
+        """Test method add(a, b)"""
+        print("add")
+        self.assertEqual(3, add(1, 2))
+        self.assertNotEqual(3, add(2, 2))
+
+    def test_minus(self):
+        """Test method minus(a, b)"""
+        print("minus")
+        self.assertEqual(1, minus(3, 2))
+
+    def test_multi(self):
+        """Test method multi(a, b)"""
+
+        self.skipTest("hhhhhh")
+        print("multi")
+
+        self.assertEqual(6, multi(2, 3))
+
+    #unittest.skip(reason)、unittest.skipIf(condition, reason)、unittest.skipUnless(condition, reason)
+    @unittest.skip("I don't want to run this case.")
+    def test_divide(self):
+        """Test method divide(a, b)"""
+        print("divide")
+        self.assertEqual(2, divide(6, 3))
+        self.assertEqual(2.5, divide(5, 2))
+
+if __name__ == '__main__':
+    unittest.main()
