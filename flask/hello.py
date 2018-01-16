911abe218958b08dd7610318f56e91f88e07d75a
daewood <daewood@qq.com>
2018-01-16 22:11:13 +0000

    flash

--- flask/hello.py
+++ flask/hello.py
@@ -1,8 +1,33 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
+app = Flask(__name__, static_path='/static')
 manager=Manager(app)
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/user/<id>')
 def user(id) :
@@ -10,8 +35,8 @@ def user(id) :
     return render_template('user.html',name=id)
 
 @app.route('/')
-def hello():
-    return "Hello, Flask!"
+def index():
+    return render_template('index.html',current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
